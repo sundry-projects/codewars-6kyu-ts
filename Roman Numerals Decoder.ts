14b70bdc1a6bdd018fd20ba24a8ed1785d74b110
joyfulpebble <96781410+joyfulpebble@users.noreply.github.com>
2023-02-06 21:08:32 +0300

    Roman Numerals Decoder

---
+++ Roman Numerals Decoder.ts
@@ -0,0 +1,44 @@
+/*
+
+Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer. 
+  You don't need to validate the form of the Roman numeral.
+
+Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, 
+  starting with the leftmost digit and skipping any 0s. 
+  So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII). 
+  The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.
+
+Example:
+
+  solution('XXI'); // should return 21
+
+Help:
+
+Symbol    Value
+  I          1
+  V          5
+  X          10
+  L          50
+  C          100
+  D          500
+  M          1,000
+
+*/
+
+function solution(roman: string): number {
+  if(roman === '') return 0;
+  type anyKeysObject = { [key: string]: number; }
+  
+  const romanNumerals: anyKeysObject = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000};
+  const exceptions = ["CD", "CM", "XL", "XC", "IV", "IX"];
+  let result = 0;
+  
+  for(let i = 0; i < roman.length; i++) {
+    exceptions.indexOf(roman[i] + roman[i + 1]) === -1 
+      ? result += romanNumerals[roman[i]]
+      : result -= romanNumerals[roman[i]]
+  }
+  
+  console.log(result)
+  return result;
+}
